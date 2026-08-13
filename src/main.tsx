958856a53debda8f6e161fb2f58d49d1dba9d1a5
Menelik Admasu <minilover2014@gmail.com>
2026-08-13 11:59:32 +0300

    fix: remove aggressive siteProtection (blocks select/F12/devtools)
    Improves UX; anti-clone tricks are ineffective and break normal use.

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,11 @@
-
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from "next-themes"
-import { initSiteProtection } from './utils/siteProtection'
 import { initAdInjectionGuard } from './utils/adInjectionGuard'
 import { registerServiceWorker } from './utils/registerSW'
 
-// Initialize site protection (only in production)
-if (import.meta.env.PROD) {
-  initSiteProtection();
-}
-
-// Always install ad-injection hooks (activated fully when player opens)
+// Host-page ad/popup hooks (fully active while Watch Now is open)
 initAdInjectionGuard();
 
 registerServiceWorker();
