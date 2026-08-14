b9fa203481b5d98169d20bdd2f93e0653aaa3945
Menelik Admasu <minilover2014@gmail.com>
2026-08-14 10:06:00 +0300

    perf: do not install adInjectionGuard until Watch Now opens

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from "next-themes"
-import { initAdInjectionGuard } from './utils/adInjectionGuard'
 import { registerServiceWorker } from './utils/registerSW'
 
-// Host-page ad/popup hooks (fully active while Watch Now is open)
-initAdInjectionGuard();
-
+// adInjectionGuard installs only when LiveWatchModal calls activateAdInjectionGuard()
 registerServiceWorker();
 
 createRoot(document.getElementById("root")!).render(
